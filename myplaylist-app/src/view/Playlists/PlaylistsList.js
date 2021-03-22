4e80325a9bd5a6e1399d850b458f9b9bb8c49d50
Tamás László <tlaszlods@gmail.com>
2021-03-22 21:19:55 +0100

    PlaylistList takarítás

--- myplaylist-app/src/view/Playlists/PlaylistsList.js
+++ myplaylist-app/src/view/Playlists/PlaylistsList.js
@@ -1,14 +1,10 @@
 import PropTypes from "prop-types";
 
-// const playlists = ["Heavy Metal", "Classics", "Movie Soundtracks"];
-
-// const PlaylistsList = (props) => {
 const PlaylistsList = ({ playlists }) => {
   return (
     <div className="ui six wide column">
       <h3>Playlists</h3>
       <div className="ui very relaxed selection list">
-        {/* {props.playlists.map((playlist) => ( */}
         {playlists.map((playlist) => (
           <div key={playlist.id} className="item">
             <i className="large compact disc middle aligned icon"></i>
@@ -18,27 +14,6 @@ const PlaylistsList = ({ playlists }) => {
             </div>
           </div>
         ))}
-        {/* <div className="item">
-          <i className="large compact disc middle aligned icon"></i>
-          <div className="content">
-            <div className="header">Heavy Metal</div>
-            <div className="description">5 songs</div>
-          </div>
-        </div>
-        <div className="active item">
-          <i className="large compact disc middle aligned icon"></i>
-          <div className="content">
-            <div className="header">Classics</div>
-            <div className="description">4 songs</div>
-          </div>
-        </div>
-        <div className="item">
-          <i className="large compact disc middle aligned icon"></i>
-          <div className="content">
-            <div className="header">Movie Soundtracks</div>
-            <div className="description">9 songs</div>
-          </div>
-        </div> */}
         <div className="item" id="newPlaylist">
           <i className="large green plus middle aligned icon"></i>
           <div className="content">
@@ -54,7 +29,7 @@ const PlaylistsList = ({ playlists }) => {
 PlaylistsList.propTypes = {
   playlists: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number,
+      id: PropTypes.number.isRequired,
       title: PropTypes.string,
       tracks: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
     })
