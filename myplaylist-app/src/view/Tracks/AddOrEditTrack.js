cabecf307efe647aed2dd7b86e8d39ecbfdd4051
Tamás László <tlaszlods@gmail.com>
2021-03-25 17:23:09 +0100

    AddOrEditTrack.js className átalakítás

--- myplaylist-app/src/view/Tracks/AddOrEditTrack.js
+++ myplaylist-app/src/view/Tracks/AddOrEditTrack.js
@@ -3,32 +3,28 @@ const AddOrEditTrack = () => {
     <div className="ui modal">
       <i className="close icon"></i>
       <div className="header">Add new Track</div>
-      <div className="image content">
+      <div className="content">
         <div className="description">
-          <form className="ui form">
-            <div className="two fields">
-              <div className="field">
-                <label>Author</label>
-                <input required type="text" placeholder="John Williams" />
-              </div>
-              <div className="field">
-                <label>Track name</label>
-                <input required type="text" placeholder="Imperial March" />
-              </div>
+          <form className="ui form three column grid">
+            <div className="field">
+              <label>Author</label>
+              <input required type="text" placeholder="John Williams" />
             </div>
-            <div className="three fields">
-              <div className="field">
-                <label>Spotify URL</label>
-                <input type="text" placeholder="https://" />
-              </div>
-              <div className="field">
-                <label>Lyrics URL</label>
-                <input type="text" placeholder="https://" />
-              </div>
-              <div className="field">
-                <label>Guitar tab URL</label>
-                <input type="text" placeholder="https://" />
-              </div>
+            <div className="field">
+              <label>Track name</label>
+              <input required type="text" placeholder="Imperial March" />
+            </div>
+            <div className="field">
+              <label>Spotify URL</label>
+              <input type="text" placeholder="https://" />
+            </div>
+            <div className="field">
+              <label>Lyrics URL</label>
+              <input type="text" placeholder="https://" />
+            </div>
+            <div className="field">
+              <label>Guitar tab URL</label>
+              <input type="text" placeholder="https://" />
             </div>
           </form>
         </div>
