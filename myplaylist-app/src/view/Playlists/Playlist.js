6463db630c057d8cf0502ffcd30a52c20facd7a2
Tamás László <tlaszlods@gmail.com>
2021-03-25 18:14:57 +0100

    trackId állapot

--- myplaylist-app/src/view/Playlists/Playlist.js
+++ myplaylist-app/src/view/Playlists/Playlist.js
@@ -1,12 +1,17 @@
 import PropTypes from "prop-types";
+import cn from "classnames";
 
-const Playlist = ({ title, tracks }) => {
+const Playlist = ({ title, tracks, onSelect, selected }) => {
   return (
     <div className="ui ten wide column">
       <h3>{title}</h3>
       <div className="ui very relaxed selection list">
         {tracks.map(({ id, artist, title }) => (
-          <div key={id} className="item">
+          <div
+            key={id}
+            className={cn("item", { active: id === selected })}
+            onClick={() => onSelect(id)}
+          >
             <i className="large music middle aligned icon"></i>
             <div className="content">
               <div className="header">{title}</div>
@@ -20,6 +25,7 @@ const Playlist = ({ title, tracks }) => {
 };
 
 Playlist.propTypes = {
+  selected: PropTypes.number,
   title: PropTypes.string,
   tracks: PropTypes.arrayOf(
     PropTypes.shape({
@@ -28,6 +34,7 @@ Playlist.propTypes = {
       title: PropTypes.string,
     })
   ).isRequired,
+  onSelect: PropTypes.func.isRequired,
 };
 
 export default Playlist;
--- myplaylist-app/src/view/Playlists/Playlists.js
+++ myplaylist-app/src/view/Playlists/Playlists.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { examplePlaylists } from "../../domain/playlist";
 import { exampleTracks } from "../../domain/track";
@@ -11,16 +11,23 @@ const examplePlaylist = examplePlaylists[0];
 const exampleTrack = exampleTracks[0];
 
 const Playlists = () => {
+  const [trackId, setTrackId] = useState(null);
+  const track = exampleTracks.find((track) => track.id === trackId);
+
   return (
     <React.Fragment>
       <div className="ui container">
         <h1>My Playlists</h1>
         <div className="ui stackable two column grid">
           <PlaylistsList playlists={examplePlaylists} />
-          <Playlist {...examplePlaylist} />
+          <Playlist
+            {...examplePlaylist}
+            onSelect={setTrackId}
+            selected={trackId}
+          />
         </div>
         <div className="ui divider"></div>
-        <TrackDetails {...exampleTrack} />
+        <TrackDetails {...track} />
       </div>
 
       <AddNewPlaylist />
